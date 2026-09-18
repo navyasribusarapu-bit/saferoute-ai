4ee9df79c677774aec75fc4da048ab1f6a58ca16
navyasribusarapu-bit <navyasribusarapu@gmail.com>
2026-09-18 10:18:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,38 +3,33 @@ import streamlit as st
 st.title("SafeRoute AI - Road Safety")
 st.success("App is LIVE!")
 st.write("Final Year Project")
+
 name = st.text_input("Enter Name")
-
-if st.button("Start"):
-    st.balloons()
-    st.write(f"Welcome {name}")
-
-# Vehicle Type - Bike, Car, Truck, Bus
 vehicle_type = st.selectbox("Select Vehicle Type 🚗", ["Bike", "Car", "Truck", "Bus"])
-
-st.success(f"Vehicle Selected: {vehicle_type}")
-
 weather = st.selectbox("Weather", ["Clear","Rainy","Foggy"])
 speed = st.slider("Speed", 20, 120, 60)
-risk = 0
 
-if weather != "Clear":
-    risk += 40
-if speed > 80:
-    risk += 30
-if vehicle_type == "Bike":
-    risk += 20
-if vehicle_type == "Truck":
-    risk += 15
-if vehicle_type == "Bus":
-    risk += 10
-
-st.write(f"Risk Level: {risk}%")
-st.write(f"Vehicle: {vehicle_type}")
-
-if risk < 30:
-    st.success("LOW RISK")
-elif risk < 60:
-    st.warning("MEDIUM RISK")
-else:
-    st.error("HIGH RISK")
+if st.button("Submit"):
+    st.balloons()
+    risk = 0
+    
+    if weather != "Clear":
+        risk += 40
+    if speed > 80:
+        risk += 30
+    if vehicle_type == "Bike":
+        risk += 20
+    if vehicle_type == "Truck":
+        risk += 15
+    if vehicle_type == "Bus":
+        risk += 10
+    
+    st.write(f"Hi {name} | Vehicle: {vehicle_type} | Risk: {risk}%")
+    
+    if risk < 30:
+        st.success("✅ YOU ARE SAFE - Have a safe journey!")
+        st.balloons()
+    elif risk < 60:
+        st.warning("⚠️ BE CAREFUL - Drive slowly!")
+    else:
+        st.error("🚨 BE CAREFUL - HIGH RISK! Avoid travel or drive very slowly!")
